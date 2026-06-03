460b6a7e47b4ebb8fcb36a4a1f9b31ee7c2c8ec5
Aymane Bouljam <235529830+aymanebouljam@users.noreply.github.com>
2026-06-03 01:30:56 +0100

    feat(backend): add prisma module setup

---
+++ backend/prisma/prisma.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private readonly configService: ConfigService) {
+    const databaseUrl = configService.getOrThrow<string>('DATABASE_URL');
+    if (!databaseUrl) {
+      throw new Error('DATABASE_URL is not defined');
+    }
+    const adapter = new PrismaPg({
+      connectionString: databaseUrl,
+    });
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
